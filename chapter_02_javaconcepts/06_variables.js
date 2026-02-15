2d47fb87d1d1c655dc292680d46a9b04910080ec
pavan <navgiresayali030@gmail.com>
2026-02-15 16:39:16 +0530

    Sync workspace

---
+++ chapter_02_javaconcepts/06_variables.js
@@ -0,0 +1,20 @@
+var a=10;
+a=20;
+console.log(a);
+//10 is a literal
+//vaiable name =identifier
+var name="Sayali";
+console.log(name);
+function print()
+{
+    var a=30;
+    console.log(a);
+
+}
+function print1()
+{
+    var a=40;
+    console.log(a);
+}
+print();
+print1();
